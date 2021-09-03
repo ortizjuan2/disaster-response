30ba29509c2b163f0c53f7c97cb3eee98f738cf6
jom <jom@mwx129.local>
2021-09-02 21:18:49 -0500

    feature: first commit
    first commit

---
+++ data/process_data.py
@@ -0,0 +1,75 @@
+import sys
+import pandas as pd
+from sqlalchemy import create_engine
+
+
+def load_data(messages_filepath, categories_filepath):
+    """ load messages and categories, create a dataset merging both files with id as key
+    """
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categories_filepath)
+    # concat both file
+    df = messages.merge(categories, on='id', how='left')
+
+    return df
+
+def clean_data(df):
+    """ clean dataset, convert categories to numeric values
+    """
+    # first row as column's name for categories
+    df.columns = list(df.columns[:5]) + list(df.iloc[0, 5:])
+    
+    categories = df.iloc[:, 5:]
+
+    for column in categories:
+        # set each value to be the last character of the string
+        categories[column] = categories[column].apply(lambda s: int(s[-1]))
+        # convert column from string to numeric
+        categories[column] = pd.to_numeric(categories[column])
+    # drop original categories
+    df = df.iloc[:, :4]
+
+    df = pd.concat([df, categories], axis='columns')
+
+    #drop duplicates
+    df = df.drop_duplicates()
+
+    return df
+
+
+def save_data(df, database_filename):
+    """ save the clean dataset to a sqlite database
+    """  
+    engine = create_engine('sqlite:///'+database_filename)
+    df.to_sql('categories', engine, index=False)
+
+
+def main():
+    if len(sys.argv) == 4:
+
+        messages_filepath, categories_filepath, database_filepath = sys.argv[1:]
+
+        print('Loading data...\n    MESSAGES: {}\n    CATEGORIES: {}'
+              .format(messages_filepath, categories_filepath))
+        df = load_data(messages_filepath, categories_filepath)
+
+        print('Cleaning data...')
+        df = clean_data(df)
+        
+        print('Saving data...\n    DATABASE: {}'.format(database_filepath))
+        save_data(df, database_filepath)
+        
+        print('Cleaned data saved to database!')
+    
+    else:
+        print('Please provide the filepaths of the messages and categories '\
+              'datasets as the first and second argument respectively, as '\
+              'well as the filepath of the database to save the cleaned data '\
+              'to as the third argument. \n\nExample: python process_data.py '\
+              'disaster_messages.csv disaster_categories.csv '\
+              'DisasterResponse.db')
+
+
+if __name__ == '__main__':
+    main()
+    
