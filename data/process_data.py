4c14ff9b370553fce8d78de1bde7db1705ad19a1
jom <jom@mwx129.local>
2021-09-06 18:16:00 -0500

    fixing replace database file if exist
    replacing database file if exist when processing data

--- data/process_data.py
+++ data/process_data.py
@@ -65,7 +65,7 @@ def save_data(df, database_filename):
         database_filename: str, database file path
     """
     engine = create_engine('sqlite:///'+database_filename)
-    df.to_sql('categories', engine, index=False)
+    df.to_sql('categories', engine, index=False, if_exists='replace')
 
 
 def main():
